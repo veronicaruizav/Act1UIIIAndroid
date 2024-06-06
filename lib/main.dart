9b97419df965d0962bbc70d6eb27642234c15cd9
VERONICA_ESPERANZA_RUIZ_AVILA <a.21308051280550@cbtis128.edu.mx>
2024-06-06 16:08:31 +0000

    actua

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,133 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const Login());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      title: 'Login AccessPhone ',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://raw.githubusercontent.com/veronicaruizav/imagenesandroid/main/fondo.jpg'),
+                fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: ListView(
+            children: [
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://raw.githubusercontent.com/veronicaruizav/imagenesandroid/main/logo.png',
+                    width: 80,
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(30),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.blue,
+                            blurRadius: 10,
+                            offset: Offset(0, 4))
+                      ]),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text('AccessPhone',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Color(0xff13334d))),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        child: const Text('Un gusto de volverte a ver',
+                            style: TextStyle(fontSize: 20)),
+                      ),
+                      const SizedBox(height: 30),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+                        child: TextField(
+                          autofocus: false,
+                          decoration: InputDecoration(
+                              hintText: 'Usuario',
+                              prefixIcon: Icon(Icons.person),
+                              contentPadding: EdgeInsets.all(10),
+                              hintStyle:
+                                  TextStyle(color: Colors.grey, fontSize: 18),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 2))),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+                        child: TextField(
+                          autofocus: false,
+                          decoration: InputDecoration(
+                              hintText: 'Contraseña',
+                              prefixIcon: Icon(Icons.lock),
+                              contentPadding: EdgeInsets.all(10),
+                              hintStyle:
+                                  TextStyle(color: Colors.grey, fontSize: 18),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 2))),
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(20),
+                                child: Text(
+                                  'Inicia Sesión',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white),
+                                ),
+                              )
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.blueGrey,
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20))),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
